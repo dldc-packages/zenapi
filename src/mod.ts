96bece9111404c26ea664116209a6acfd7b3b648
Etienne Deladonchamps <e.deladonchamps@gmail.com>
2023-08-02 22:27:11 +0200

    Export types

--- src/mod.ts
+++ src/mod.ts
@@ -1,6 +1,34 @@
 export { ApiContext } from './context';
-export { engine, resolve } from './engine';
+export { engine, resolve, type IEngine, type IModelResolved } from './engine';
 export { InvalidQuery, InvalidResolvedValue, UnresolvedValue } from './erreur';
-export { implem, withCtx } from './implem';
-export { query } from './query';
-export { schema } from './schema';
+export {
+  extractImpleResult,
+  implem,
+  withCtx,
+  type IImplemFnData,
+  type IImplementation,
+  type TImplemFn,
+  type TImplemFnResponse,
+} from './implem';
+export {
+  createQuery,
+  query,
+  type IQuery,
+  type TInternalQueryDefObject,
+  type TModelQueryDef,
+  type TQueryAny,
+  type TQueryDef,
+  type TQueryDefItem,
+  type TQueryResult,
+  type TQuerySelect,
+  type TQuerySelectResult,
+} from './query';
+export {
+  schema,
+  type IInputDef,
+  type IListQueryBuilder,
+  type TInputQueryBuilder,
+  type TListDef,
+  type TModelsRecord,
+  type TRecordQueryBuilder,
+} from './schema';
