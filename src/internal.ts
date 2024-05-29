84d8de5772af7398c12a8cc1bc6f7e6ab56f2cc1
Etienne Deladonchamps <e.deladonchamps@gmail.com>
2024-05-29 21:52:38 +0200

    Migrate to JSR

--- src/internal.ts
+++
@@ -1,7 +0,0 @@
-export const INTERNAL = Symbol('INTERNAL');
-export const QUERY_DATA = Symbol('DATA');
-
-export const RESULT = Symbol('RESULT');
-export type RESULT = typeof RESULT;
-
-export const RESOLVER = Symbol('RESOLVER');
